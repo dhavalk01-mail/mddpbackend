bc2bfd6fa621c07129afc59141a9c2a1c67a07a7
dhavalk01-mail <155353848+dhavalk01-mail@users.noreply.github.com>
2024-09-05 09:17:03 +0000

    fix cors error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 import express from "express";
 import connectDB from "./db.js";
 import router from "./serviceRoutes.js";
@@ -5,6 +6,10 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express"
 
 const app = express();
+
+// Allow request from any source. In real production, this should be limited to allowed origins only
+app.use(cors());
+
 app.use(express.json());
 connectDB();
 app.use("/api" , router);
