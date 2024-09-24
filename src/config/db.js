4f7a82017d6172dca821e651e6beb369eeb0bab7
dhavalk01-mail <155353848+dhavalk01-mail@users.noreply.github.com>
2024-09-24 07:12:25 +0000

    v2.1

--- src/config/db.js
+++ src/config/db.js
@@ -17,8 +17,8 @@ const connectDB = async () => {
             // var mongourl = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
             var mongourl = MONGODB_PROD_URL;
         } else {
-            var mongourl = "mongodb+srv://dhavalkoradiya:1zsuaG058yFKCxJ6@cluster0.ambhqqq.mongodb.net/services";
-            // var mongourl = MONGODB_DEV_URL;
+            // var mongourl = "mongodb+srv://dhavalkoradiya:1zsuaG058yFKCxJ6@cluster0.ambhqqq.mongodb.net/services";
+            var mongourl = MONGODB_DEV_URL;
         }
         // console.log(mongourl);
         await mongoose.connect(mongourl)
